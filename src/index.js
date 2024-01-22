9d17bdf65389b8a5eee570e022ffb8a69482ad22
Caleb Siegel <caleb.siegel@gmail.com>
2024-01-22 12:47:27 -0500

    final edits

--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
-// write your code here 
+// declare variables for html elements
+let menu = document.querySelector(`#ramen-menu`)
+let featuredImage = document.querySelector(`.detail-image`)
+let featuredName = document.querySelector(`.name`)
+let featuredRestaurant = document.querySelector(`.restaurant`)
+let featuredRating = document.querySelector(`#rating-display`)
+let featuredComment = document.querySelector(`#comment-display`)
+let createName = document.querySelector("#new-name")
+let createRestaurant = document.querySelector("#new-restaurant")
+let createImage = document.querySelector("#new-image")
+let createRating = document.querySelector("#new-rating")
+let createComment = document.querySelector("#new-comment")
+let createRamen = document.querySelector("#new-ramen")
+let editRamen = document.querySelector(`#edit-ramen`)
+
+// create function for changing the data in the featured section
+function changeData(item) {
+    featuredImage.src = item['image'];
+    featuredName.textContent = item[`name`];
+    featuredRestaurant.textContent = item[`restaurant`];
+    featuredRating.textContent = item[`rating`];
+    featuredComment.textContent = item[`comment`];
+};
+
+// default the first ramen to the main image
+document.addEventListener(`DOMContentLoaded`, (event) => {
+    fetch("http://localhost:3000/ramens/")
+    .then(response => response.json())
+    .then(ramens => {
+        event.preventDefault()
+        changeData(ramens[0])
+    })
+})
+
+// add the images to the top and allow functionality to click on the image and then see the full data in the main section
 fetch("http://localhost:3000/ramens")
 .then(response => response.json())
 .then(ramens => {
     ramens.forEach(ramen => {
         let img = document.createElement(`img`)
-        img.setAttribute(`src`, ramen[`image`])
-        img.setAttribute(`id`, ramen[`name`])
-        document.querySelector(`#ramen-menu`).append(img)    
-        
+        img.src = ramen[`image`]
+        img.id = ramen[`name`]
+        menu.append(img)
+                
         img.addEventListener(`click`, (event) => {
             event.preventDefault()
-                    document.querySelector(`.detail-image`).setAttribute(`src`, ramen[`image`])
-                    document.querySelector(`.name`).textContent = ramen[`name`]
-                    document.querySelector(`.restaurant`).textContent = ramen[`restaurant`]
-                    document.querySelector(`#rating-display`).textContent = ramen[`rating`]
-                    document.querySelector(`#comment-display`).textContent = ramen[`comment`]
+            changeData(ramen)
         })
         })
     })    
 
-document.querySelector("#new-ramen").addEventListener("submit", (event) => {
+// Add new ramen to the list and make it clickable
+createRamen.addEventListener("submit", (event) => {
     event.preventDefault()
     fetch("http://localhost:3000/ramens", {
         method: "POST",
@@ -28,18 +59,52 @@ document.querySelector("#new-ramen").addEventListener("submit", (event) => {
             "Accept": "application/json"
         },
         body: JSON.stringify({
-            name: document.querySelector("#new-name").value,
-            restaurant: document.querySelector("#new-restaurant").value,
-            image: document.querySelector("#new-image").value,
-            rating: document.querySelector("#new-rating").value,
-            comment: document.querySelector("#new-comment").value
+            name: createName.value,
+            restaurant: createRestaurant.value,
+            image: createImage.value,
+            rating: createRating.value,
+            comment: createComment.value
         })
     })
     .then(response => response.json())
     .then(newRamen => {
         let newImg = document.createElement(`img`)
-        newImg.setAttribute(`src`, newRamen.image)
-        newImg.setAttribute(`id`, newRamen[`name`])
-        document.querySelector(`#ramen-menu`).append(newImg)
+        newImg.src = newRamen.image
+        newImg.id = newRamen[`name`]
+        menu.append(newImg)
+        
+        newImg.addEventListener(`click`, (event) => {
+            event.preventDefault()
+            changeData(newRamen)
+        })
     })
 })
+
+// Update featured ramen's rating and comment
+
+// editRamen.addEventListener(`submit`, (event) => {
+//     event.preventDefault()
+//     console.log(document.querySelector("#new-comment").value)
+//     let newRating = document.querySelector(`#new-rating`)
+//     featuredRating.textContent = newRating.value
+    // featuredComment.textContent = document,querySelector(`#new-comment`)
+// })
+
+// editRamen.addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     fetch(`http://localhost:3000/ramens/:id`, {
+//         method: "PATCH",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             comment: document.querySelector(`#new-comment`).value,
+//             rating: document.querySelector(`#new-rating`).value
+//         })
+//     })
+//     .then(response => response.json())
+//     .then(editedRamen => {
+//         changeData(newRamen)
+//     })
+// })
